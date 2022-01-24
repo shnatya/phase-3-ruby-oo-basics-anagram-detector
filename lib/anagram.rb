053725bf0d8b9d221198eabb0f76774d158fa10a
Anastasia Shatokhina <shnatya@gmail.com>
2022-01-24 12:50:37 -0800

    commit tree 15bce2494f230d631d8e42e2428d46a50c10419f

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,21 @@
 # Your code goes here!
+require "pry"
+
+class Anagram
+    def initialize(word)
+        @word=word
+    end
+
+    def match(arr)
+        result = []
+        arr.each do |w|
+            if w.length == @word.length
+                if w.chars.sort == @word.chars.sort
+                    result << w
+                end
+            end
+        end
+        result
+    end
+end
+
